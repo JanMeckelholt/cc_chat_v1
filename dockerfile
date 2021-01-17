b7a7a709fde9b0620f69497ee91d97eb4bd17b4b
JanMeckelholt <Jan_Meckelholt@web.de>
2021-01-17 12:46:01 +0100

    chat speaks to mongo :-)

--- dockerfile
+++ dockerfile
@@ -1,4 +1,7 @@
 FROM node
 
 COPY . /root/
-CMD cd /root/ && npm start
+
+CMD cd /root/ && npm start 
+##&& node monog-init.js
+#CMD cd /root/ && node monog-init.js
--- index.js
+++ index.js
@@ -6,8 +6,26 @@ const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+// Connection URL
+const url = 'mongodb://172.17.0.1:27017/data/db';
+// Database Name
+const dbName = 'chat-db';
+// Create a new MongoClient
+const client = new MongoClient(url);
+
 server.listen(port, () => {
   console.log('Server listening at port %d', port);
+
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+  const db = client.db(dbName);
+  client.close();
+});
+
 });
 
 // Routing
